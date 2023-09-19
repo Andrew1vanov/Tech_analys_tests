837aa8f27103fb21cd6d2b3adece81530a1535e7
Andrew Ivanov <andrew11.12.97@mail.ru>
2023-09-19 20:05:54 +0300

    add functions

--- first_test.py
+++ first_test.py
@@ -1,4 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import apimoex
+import requests
 
-a = np.arange(5)
+def load_data(security: str):
+    data = apimoex.get_board_history(requests.Session(), security, columns= ['CLOSE'], board = 'TQBR', 
+                                     start = '2023-08-01', end = '2023-09-01')
+    df = pd.DataFrame(data)
+    price = df.iloc[:].values
+    data2 = apimoex.get_market_history(requests.Session(), security, columns=['CLOSE'], 
+                                       start = '2023-08-01', end = '2023-09-01')
+    price2 = pd.DataFrame(data2).iloc[:].values
+    #plt.plot(price)
+    plt.plot(price2)
+    plt.show()
+
+load_data('GAZP')
+
+def guns_angels():
+    print('cal')
